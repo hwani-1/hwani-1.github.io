fcfd0d6c4d7f90ad3cfcaccda5c96e95f5f7f4b4
hwani-1 <ji581520@gmail.com>
2021-06-29 01:09:12 +0900

    timer correction and more

--- nyushitimer.js
+++ nyushitimer.js
@@ -1,38 +1,35 @@
 const year = new Date().getFullYear();
-const kindaiexam = new Date(2022, 0,28).getTime();
+const kindaiexam = new Date(2022, 0,29).getTime();
 const month = new Date().getMonth();
 
-// countdown
+// 카운트다운
 let timer = setInterval(function() {
 
-  // get today's date
+  // 오늘 날짜
   const today = new Date().getTime();
 
-  // get the difference
+  // 차이 계산
   let diff;
   {
     diff = kindaiexam - today;
   }
 
-  // math
-  let months = Math.floor(diff / (1000 * 60 * 60 * 60 * 12));
-  let days = Math.floor((diff % (1000 * 60 * 60 * 60 * 12)) / (1000 * 60 * 60 * 60));
+  // 밀리초 변환
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
   let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-  // display
-  document.getElementById("timer").innerHTML =
-    "<div class=\"months\"> \
-  <div class=\"numbers\">" + months + "</div>ヶ月</div> \
-<div class=\"days\"> \
-  <div class=\"numbers\">" + days + "</div>日</div> \
-<div class=\"hours\"> \
-  <div class=\"numbers\">" + hours + "</div>時間</div> \
-<div class=\"minutes\"> \
-  <div class=\"numbers\">" + minutes + "</div>分</div> \
-<div class=\"seconds\"> \
-  <div class=\"numbers\">" + seconds + "</div>秒</div> \
-</div>";
+  // 표시
+    document.getElementById("timer").innerHTML =
+      "<div class=\"days\"> \
+    <div class=\"numbers\">" + days + "</div>日</div> \
+  <div class=\"hours\"> \
+    <div class=\"numbers\">" + hours + "</div>時間</div> \
+  <div class=\"minutes\"> \
+    <div class=\"numbers\">" + minutes + "</div>分</div> \
+  <div class=\"seconds\"> \
+    <div class=\"numbers\">" + seconds + "</div>秒</div> \
+  </div>";
 
 }, 1000);
